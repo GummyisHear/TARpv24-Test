ac1d96f819aa18258d5174aefdbb61923c76baa0
Opilane TTHK <artjompoldsaar@gmail.com>
2025-02-03 14:52:59 +0200

    if else code

--- IfElseNesting/Program.cs
+++ IfElseNesting/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace IfElseNesting
+﻿namespace IfElseNesting;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Number Control!");
+
+        int number = 201;
+
+        //kontrollitakse kas arv on suurem 189-st
+        if (number > 189)
         {
-            Console.WriteLine("Hello, World!");
+            //kui vastab if tingimustele, siis n'itab konsoolis seda arvu
+            Console.WriteLine("Number is bigger");
+
+            //if ja else nestimine
+            if (number >= 190 && number <= 200)
+            {
+                Console.WriteLine("Number is between 190 to 200");
+            }
+            else
+            {
+                Console.WriteLine("Number is bigger than 200");
+            }
         }
+        else
+        {
+            Console.WriteLine("Number is smaller");
+        }
+    }
+
+    private static void Error(object text)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Red;
+        var now = DateTime.Now;
+        var timeString = now.ToLongTimeString() + ":" + now.Millisecond;
+        Console.WriteLine(now.ToLongTimeString().PadRight(10) + " ERROR".PadRight(10) + text.ToString());
+        Console.ResetColor();
     }
 }
