ddcc677f99f90d19e14afae6caa0cfd8a78eb6ae
ImAxiz <artjompoldsaar@gmail.com>
2025-02-05 14:58:31 +0200

    coin calc

---
+++ CoinCalculator/CoinCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CoinCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var earthRadiusKm = 6378;
+            var coinDiameterMm = 25.75f;
+
+            var earthLengthKm = 2 * earthRadiusKm * MathF.PI;
+            var earthLengthMm = 1_000_000 * earthLengthKm;
+
+            var totalCoins = earthLengthMm / coinDiameterMm;
+
+            Console.WriteLine("Coins: " + totalCoins);
+
+        }
+    }
+}
