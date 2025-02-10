dd9ab0ba06e38c9e49e0affe7fa889feb8b699ed
Opilane TTHK <artjompoldsaar@gmail.com>
2025-02-10 14:14:33 +0200

    pyramid

---
+++ Pyramid/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loo püramiid tärnidest:");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Sisesta püramiidi suurus");
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i <= n; i++)
+            {
+                for (var j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (var j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ SwitchCoinDivider/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SwitchCoinDivider
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input coins:");
+            var coins = int.Parse(Console.ReadLine());
+
+            var fifty = coins / 50;
+            coins -= fifty * 50;
+
+            var twenty = coins / 20;
+            coins -= twenty * 20;
+
+            var ten = coins / 10;
+            coins -= ten * 10;
+
+            var five = coins / 5;
+            coins -= five * 5;
+
+            var two = coins / 2;
+            coins -= two * 2;
+
+            var one = coins;
+
+            Console.WriteLine("You have: ");
+            if (fifty > 0)
+                Console.WriteLine($"{fifty} - 50 cent coins");
+            if (twenty > 0)
+                Console.WriteLine($"{twenty} - 20 cent coins");
+            if (ten > 0)
+                Console.WriteLine($"{ten} - 10 cent coins");
+            if (five > 0)
+                Console.WriteLine($"{five} - 5 cent coins");
+            if (two > 0)
+                Console.WriteLine($"{two} - 2 cent coins");
+            if (one > 0)
+                Console.WriteLine($"{one} - 1 cent coins");
+        }
+    }
+}
