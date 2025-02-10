299915a5b8fc62f183a98dba2641363eae14652d
Opilane TTHK <artjompoldsaar@gmail.com>
2025-02-10 13:16:24 +0200

    if else calculator

---
+++ IfElseCalculator/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace IfElseCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert first number and press enter:");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insert calculation type and press enter:");
+            var calculateType = Console.ReadLine();
+
+            Console.WriteLine("Insert second number and press enter:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double result = 0;
+            if (calculateType == "1")
+            {
+                result = num1 + num2;
+                Console.WriteLine($"{num1} + {num2} = {result}");
+            }
+            else if (calculateType == "2")
+            {
+                result = num1 - num2;
+                Console.WriteLine($"{num1} - {num2} = {result}");
+            }
+            else if (calculateType == "3")
+            {
+                result = num1 * num2;
+                Console.WriteLine($"{num1} * {num2} = {result}");
+            }
+            else if (calculateType == "4")
+            {
+                if (num2 == 0)
+                {
+                    Console.WriteLine("CANNOT DIVIDE BY ZERO!");
+                    return;
+                }
+                result = num1 / num2;
+                Console.WriteLine($"{num1} / {num2} = {result}");
+            }
+            else
+            {
+                Console.WriteLine("ERROR!");
+            }
+        }
+    }
+}
